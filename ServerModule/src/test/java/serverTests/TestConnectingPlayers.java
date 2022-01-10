8e21d63532a4db708b5d51205e80ab2b9e9236cd
Mkanobi <maksymilian_piotrowski@wp.pl>
2022-01-10 09:16:44 +0100

    Naprawiono test TestConnectingPlayers

--- ServerModule/src/test/java/serverTests/TestConnectingPlayers.java
+++ ServerModule/src/test/java/serverTests/TestConnectingPlayers.java
@@ -1,5 +1,7 @@
 package serverTests;
+import hex.Hex;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,6 +13,8 @@ import server.gui.ServerLogDisplay;
 
 import java.io.IOException;
 
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class TestConnectingPlayers {
 
@@ -18,16 +22,21 @@ public class TestConnectingPlayers {
     PlayerHandler playerHandler;
     @Mock
     ServerLogDisplay serverLogDisplay;
+
+    @Before
+    public void setup() {
+        when(playerHandler.getColorname()).thenReturn("RED");
+    }
     @Test
     public void testGameStarting() throws IOException {
         Assert.assertNotNull(serverLogDisplay);
         GameState GS = new GameState(3,0,serverLogDisplay);
         GS.addPlayer(playerHandler);
-        Assert.assertEquals(GS.getGameStarted(),false);
+        Assert.assertFalse(GS.getGameStarted());
         GS.addPlayer(playerHandler);
-        Assert.assertEquals(GS.getGameStarted(),false);
+        Assert.assertFalse(GS.getGameStarted());
         GS.addPlayer(playerHandler);
-        Assert.assertEquals(GS.getGameStarted(),true);
+        Assert.assertTrue(GS.getGameStarted());
     }
 
 }
